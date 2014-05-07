1bc764b42f647c54e370426c6fe13ab501b2d439
Jonathan Barnes <jonathan@pivotallabs.com>
2014-05-06 22:19:45 -0700

    Moves base aliases into attributes
    - Base aliases are defined in a node sprout=>git=>base_aliases
    - Adds empte attributes node called sprout=>git=>aliases for people to add custom aliases to in soloistrc
    - adds fake aliases to soloistrc to help with testing

---
+++ attributes/aliases.rb
@@ -0,0 +1,24 @@
+pretty_format = %q{--pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'}
+node.default['sprout']['git']['base_aliases'] = [
+  'st status',
+  'di diff',
+  'co checkout',
+  'ci commit',
+  'br branch',
+  'sta stash',
+  'llog "log --date=local"',
+  'flog "log --pretty=fuller --decorate"',
+  "lg \"log --graph #{pretty_format} --abbrev-commit --date=relative\"",
+  'lol "log --graph --decorate --oneline"',
+  'lola "log --graph --decorate --oneline --all"',
+  'blog "log origin/master... --left-right"',
+  'ds "diff --staged"',
+  'fixup "commit --fixup"',
+  'squash "commit --squash"',
+  'unstage "reset HEAD"',
+  'rum "rebase master@{u}"'
+]
+
+node.default['sprout']['git']['aliases'] = [
+  #'ci pair-commit'
+]
