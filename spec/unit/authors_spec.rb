f59b15d631f7fa23fcbb9122b83ff6a48f9703d5
Brian Cunnie and Jonathan Barnes <jonathan@pivotallabs.com>
2014-05-27 21:33:56 -0700

    Adds spec for git_scripts recipe
    - Updates authors spec to assert against a string rather than a hash.to_yaml to
      improve readability

--- spec/unit/authors_spec.rb
+++ spec/unit/authors_spec.rb
@@ -5,27 +5,24 @@ describe 'sprout-git::authors' do
 
   it 'installs a template in ~/.git-authors' do
     chef_run.node.set['sprout']['git']['authors'] = [
-      {initials: 'eg', name: 'El Gringo', email: 'gringo@custom.example.com'},
-      {initials: 'hh', name: 'Hagar the Horrible'},
-      {initials: 'lg', name: 'Lady Godiva', username: 'lgodiva'},
+      { initials: 'eg', name: 'El Gringo', email: 'gringo@custom.example.com' },
+      { initials: 'hh', name: 'Hagar the Horrible' },
+      { initials: 'lg', name: 'Lady Godiva', username: 'lgodiva' }
     ]
     chef_run.node.set['sprout']['git']['domain'] = 'default.example.com'
     chef_run.node.set['sprout']['git']['prefix'] = 'unused'
 
     chef_run.converge(described_recipe)
-    expected = {
-      'authors' => {
-        'eg' => 'El Gringo',
-        'hh' => 'Hagar the Horrible',
-        'lg' => 'Lady Godiva; lgodiva'
-      },
-      'email' => {
-        'domain' => 'default.example.com'
-      },
-      'email_addresses' => {
-        'eg' => 'gringo@custom.example.com'
-      }
-    }.to_yaml.gsub(/^---\n/,'')
+    expected = <<-EOF
+authors:
+  eg: El Gringo
+  hh: Hagar the Horrible
+  lg: Lady Godiva; lgodiva
+email:
+  domain: default.example.com
+email_addresses:
+  eg: gringo@custom.example.com
+EOF
     expect(chef_run).to render_file('/home/fauxhai/.git-authors').with_content(/#{expected}/)
   end
 end
---
+++ spec/unit/git_scripts_spec.rb
@@ -0,0 +1,56 @@
+require 'unit/spec_helper'
+
+describe 'sprout-git::git_scripts' do
+  let(:chef_run) { ChefSpec::Runner.new }
+  let(:which_pair) { nil }
+  let(:untar_command) do
+    'tar --strip=2 -xzf /var/chef/cache/git_scripts.tgz -C /usr/local/bin'
+  end
+  before { stub_command('which git-pair').and_return(which_pair) }
+
+  it 'downloads git_scripts as a tarball' do
+    chef_run.converge(described_recipe)
+    expect(chef_run).to create_remote_file('/var/chef/cache/git_scripts.tgz').with(
+      source: 'https://github.com/pivotal/git_scripts/tarball/master'
+    )
+  end
+
+  it 'installs git_scripts' do
+    chef_run.converge(described_recipe)
+    expect(chef_run).to run_execute(untar_command).with(user: 'fauxhai')
+  end
+
+  context 'when the git scripts have prevoiusly been installed' do
+    let(:which_pair) { '/path/to/git-pair' }
+
+    it 'does not install the git scripts' do
+      chef_run.converge(described_recipe)
+      expect(chef_run).to_not run_execute(untar_command)
+    end
+  end
+
+  it 'installs a template in ~/.pairs' do
+    chef_run.node.set['sprout']['git']['authors'] = [
+      { initials: 'eg', name: 'El Gringo', email: 'gringo@custom.example.com' },
+      { initials: 'hh', name: 'Hagar the Horrible' },
+      { initials: 'lg', name: 'Lady Godiva', username: 'lgodiva' }
+    ]
+    chef_run.node.set['sprout']['git']['domain'] = 'default.example.com'
+    chef_run.node.set['sprout']['git']['prefix'] = 'lord'
+
+    chef_run.converge(described_recipe)
+    expected = <<-EOF
+pairs:
+  eg: El Gringo
+  hh: Hagar the Horrible
+  lg: Lady Godiva; lgodiva
+
+email:
+  prefix: lord
+  domain: default.example.com
+
+global: true
+EOF
+    expect(chef_run).to render_file('/home/fauxhai/.pairs').with_content(/#{expected}/)
+  end
+end
