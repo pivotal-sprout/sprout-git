ddb03ed3fd0da80544a6c9f0a20f196e4be0e71b
Jonathan Barnes <jonathan@codeword.net>
2015-12-15 11:49:56 -0800

    Github recursive & branch configuration enhancements
    Adds support for:
     - a `github` key following `[org]/[repo]` symantics
     - a `branch` key to checkout a specific branch
     - a `recursive` key, if `true` will add a `--recursive` flag to the
       clone command essentially cloning the repos submodles (defaults to
       false)
    
    [#100160332](https://www.pivotaltracker.com/story/show/100160332)

--- recipes/projects.rb
+++ recipes/projects.rb
@@ -7,15 +7,22 @@ node['sprout']['git']['projects'].each do |hash_or_legacy_array|
   post_clone_commands = []
   if hash_or_legacy_array.is_a?(Hash)
     project_hash = hash_or_legacy_array
-    repo_address = project_hash['url']
+    repo_address = if project_hash['url']
+      project_hash['url']
+    elsif project_hash['github']
+      "git@github.com:#{project_hash['github']}.git"
+    end
+    do_recursive = "--recursive" if project_hash['recursive']
     repo_name = project_hash['name'] || %r{^.+\/([^\/\.]+)(?:\.git)?$}.match(repo_address)[1]
     repo_dir = project_hash['workspace_path']
+    repo_branch = project_hash['branch'] || 'master'
     post_clone_commands = project_hash['post_clone_commands'] || []
   else
     legacy_array = hash_or_legacy_array
     repo_name = legacy_array[0]
     repo_address = legacy_array[1]
     post_clone_commands << 'git submodule update --init --recursive'
+    repo_branch = 'master'
   end
   repo_dir ||= "#{node['sprout']['home']}/#{node['sprout']['git']['workspace_directory']}"
   repo_dir = File.expand_path(repo_dir)
@@ -28,7 +35,16 @@ node['sprout']['git']['projects'].each do |hash_or_legacy_array|
   end
   repo_existed_originally = ::File.exist?("#{repo_dir}/#{repo_name}")
 
-  execute "git clone #{repo_address} #{repo_name}" do
+  execute [
+    'git',
+    'clone',
+    '-b',
+    repo_branch,
+    do_recursive,
+    repo_address,
+    repo_name
+  ].compact.join(' ') do
+#  execute "git clone -b #{repo_branch}#{do_recursive} #{repo_address} #{repo_name}" do
     user node['sprout']['user']
     cwd repo_dir
     not_if { ::File.exist?("#{repo_dir}/#{repo_name}") }
