0f88d42a486cd80cba8cae9ce61dda699887590d
Jonathan Barnes <jonathan@codeword.net>
2015-12-17 17:45:59 -0800

    Corrects the minor version bump
    It should have moved from 0.1.1 -> 0.2.0 not 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sprout-maintainers@googlegroups.com'
 license          'MIT'
 description      'Installs/Configures sprout-git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.0'
 supports         'mac_os_x'
 depends          'sprout-base'
 depends          'homebrew'
