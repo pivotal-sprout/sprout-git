b21a76beadfecb5caa2d761f9ce58441f57dc1f3
Christopher Brown <cbrown@pivotal.io>
2016-08-24 15:36:51 -0700

    remove cred-alert
    It doesn't belong in here.

--- recipes/cred_alert.rb
+++
@@ -1,7 +0,0 @@
-remote_file '/usr/local/bin/cred-alert-cli' do
-  source 'https://s3.amazonaws.com/cred-alert/cli/current-release/cred-alert-cli_darwin'
-  user node['sprout']['user']
-  group node['sprout']['group']
-  mode '0755'
-  action :create_if_missing
-end
